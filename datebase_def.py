fdab7343650ccb6c782c8a9aaec36244b6863a69
Miffle <miffle1337@mail.ru>
2022-08-01 10:16:55 +0300

    Datebase

---
+++ datebase_def.py
@@ -0,0 +1,34 @@
+import sqlite3
+
+
+def insert_in_db(userid, vk_user_api):
+    connection = sqlite3.connect('identifier.sqlite')
+    cursor = connection.cursor()
+    result = cursor.execute('SELECT * FROM `Users` WHERE `user_id` = ?', (userid,)).fetchall()
+    if not bool(len(result)):
+        cursor.execute("INSERT INTO `Users` VALUES(?, ?, ?);", (userid, True, vk_user_api))
+    else:
+        subscribe(userid, vk_user_api)
+    connection.commit()
+    connection.close()
+
+def subscribe(userid, vk_user_api):
+    connection = sqlite3.connect('identifier.sqlite')
+    cursor = connection.cursor()
+    cursor.execute("UPDATE `Users` SET `subscrition` = ? WHERE `user_id` = ?", (True, userid))
+    cursor.execute("UPDATE `Users` SET `VK_api` = ? WHERE `user_id` = ?", (vk_user_api, userid))
+    connection.commit()
+    connection.close()
+def unsubscribe(userid):
+    connection = sqlite3.connect('identifier.sqlite')
+    cursor = connection.cursor()
+    cursor.execute("UPDATE `Users` SET `subscrition` = ? WHERE `user_id` = ?", (False, userid))
+    connection.commit()
+    connection.close()
+
+
+def read_from_db(tgid):
+    connection = sqlite3.connect('identifier.sqlite')
+    cursor = connection.cursor()
+    cursor.execute("SELECT subscrition FROM Users WHERE user_id = ?", (tgid,))
+
--- main.py
+++ main.py
@@ -1,34 +1,56 @@
-from telebot import types
-import vk_api
 import telebot
+import vk_api
+from telebot import types
+import datebase_def
 import Info
 import last_message
-import time
-import threading
 
 session = vk_api.VkApi(token=Info.vk_TOKEN)
 vk = session.get_api()
 bot = telebot.TeleBot(Info.TGbot_token, parse_mode=None)
-
+markup1 = types.ReplyKeyboardMarkup(resize_keyboard=True)
 markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
 itembtn1 = types.KeyboardButton('Проверка сообщений')
-itembtn2 = types.KeyboardButton('Помощь')
-markup.add(itembtn1, itembtn2)
+itembtn2 = types.KeyboardButton('Подписаться')
+itembtn3 = types.KeyboardButton('Отписаться')
+itembtn4 = types.KeyboardButton('Помощь')
+markup.add(itembtn1, itembtn2, itembtn4)
+markup1.add(itembtn1, itembtn3, itembtn4)
 
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.send_message(Info.chat_id,
-                     "Привет, думаю, что ты уже знаешь функционал, но, если нет, то можешь нажать на кнопку 'Помощь' 😄",
-                     reply_markup=markup)
+    bot.send_message(message.chat_id,
+                     "Привет, думаю, что ты уже знаешь функционал, но,"
+                     " если нет, то можешь нажать на кнопку 'Помощь' 😄\n"
+                     "Начну сразу, для работы мне нужен твой API ключ, сообщения я у себя не держу и даже их не вижу\n"
+                     "Ссылка для предоставления прав - \n<span class='tg-spoiler'>https://oauth.vk.com/authorize?"
+                     "client_id=6121396&scope=593920&redirect_uri=https://oauth.vk.com/blank.html&display=page&response"
+                     "_type=token&revoke=1</span>\n\n\n"
+                     "<b>Гайд по получению api:</b>\n"
+                     "1) Переходишь по ссылке сверху\n"
+                     "2) Ознакамливаешься и нажимаешь 'Разрешить'\n"
+                     "3) Копируешь часть адресной строки от <u>access_token=</u> до <u>&expires_in</u>"
+                     " и отправляешь мне\n"
+                     "Для всего этого процесса - нажми 'Подписаться'",
+                     reply_markup=markup,
+                     parse_mode="HTML")
 
 
 @bot.message_handler(content_types=['text'])
 def support(message):
     if message.chat.type == 'private':
-        if message.text == 'Помощь':
+        if message.text == 'Подписаться':
+            send = bot.send_message(Info.chat_id, "Отправь сюда API ключ", reply_markup=markup)
+            bot.register_next_step_handler(send, get_api)
+        elif message.text == 'Отписаться':
+            bot.send_message(Info.chat_id, "Очень жаль, если что, то я всегда тут", reply_markup=markup)
+            datebase_def.unsubscribe(message.chat.id)
+
+
+        elif message.text == 'Помощь':
             bot.send_message(Info.chat_id, "lalalala", reply_markup=markup)
-        if message.text == 'Проверка сообщений':
+        elif message.text == 'Проверка сообщений':
             last_message.get_last_message(session, 10, bot, markup)
             # f = open("mes.txt", "r")
             # messages = f.read().splitlines()
@@ -36,6 +58,15 @@ def support(message):
             #     bot.send_message(Info.chat_id, messages[i], reply_markup=markup, disable_web_page_preview=True)
             #     time.sleep(0.5)
             # threading.Thread.start(last_message.messag(bot, markup, session))
+
+
+@bot.message_handler(content_types=['text'])
+def get_api(message):
+    if len(message.text) > 15:
+        userid = message.chat.id
+        vk_user_api = message.text
+        datebase_def.insert_in_db(userid, vk_user_api)
+        bot.send_message(message.chat.id, "Регистрация прошла успешно!", reply_markup=markup1)
 
 
 if __name__ == "__main__":
